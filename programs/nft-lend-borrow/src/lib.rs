e9576196e5dc6006ed1cb783af9f8fa6e8448c8a
Clarence <clarencepenz@users.noreply.github.com>
2024-06-08 20:35:48 +0100

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("8QKWwbKZWgoeXT9t9NWUyAZHVRkLTzN4zwVGxA8rjvU5");
 
 #[program]
 pub mod nft_lend_borrow {
